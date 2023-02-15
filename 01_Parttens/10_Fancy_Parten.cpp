ed0c27cc77cffbdf7b03c71cfdd747f9d50cb27a
IAkashMondal <adas085322gmail.com>
2023-02-16 01:02:14 +0530

    Fancy Patten #2

--- 01_Parttens/10_Fancy_Parten.cpp
+++ 01_Parttens/10_Fancy_Parten.cpp
@@ -25,15 +25,9 @@ int main(){
     }
 }
 
-
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cin>>n;
-//     cout <<" Add n"<<endl;
-
-//     for(int i=0;i<n;i++){
-        
-//     }
-// }
+// 1
+// 2*2
+// 3*3*3
+// 3*3*3
+// 2*2
+// 1
