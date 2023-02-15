ad6d0510a45a7f7cc02bf7a7ca6974ad0afb460f
IAkashMondal <adas085322gmail.com>
2023-02-16 02:23:40 +0530

    Template

--- 01_Parttens/0001_A_TempCodeRunnerFile.cpp
+++ 01_Parttens/0001_A_TempCodeRunnerFile.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main(){
     int n;
-    cin>> n ;
     cout <<"Add n Vallue ------------------->"<<endl;
-
+    cin>> n ;
+  
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
 
         }
+        cout << endl;
     }
 }
