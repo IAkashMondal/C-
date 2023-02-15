1831b0ecc40f4432af02bee043a74292c12a63a6
IAkashMondal <adas085322gmail.com>
2023-02-16 00:35:03 +0530

    Fllipped Solid Diamond

--- 01_Parttens/09_Flipped_Solid_Diamond.cpp
+++ 01_Parttens/09_Flipped_Solid_Diamond.cpp
@@ -1,5 +1,39 @@
 #include<iostream>
 using namespace std;
 int main(){
-cout  << "tringle-testing";
+ int n;
+ cin >> n ;
+ cout <<" incert n"<< endl;
+//   1st half------------------------------------------------>
+ for(int i=0 ;i<n;i++){
+    // half pyramid---------------->
+    for(int j =0 ;j<n-i;j++){
+        cout <<"*";
+    }
+    //  Full Pyramid with Space ---------------->
+    for(int j =0 ;j<2*i+1;j++){
+        cout <<" ";
+    }
+    // half pyramid---------------->
+    for(int j =0 ;j<n-i;j++){
+        cout <<"*";
+    }
+    cout << endl;
+ }
+//   2nd half ------------------------------------------------>
+ for(int i=0 ;i<n;i++){
+    // Inverted half pyramid---------------->
+    for(int j =0 ;j<i+1;j++){
+        cout <<"*";
+    }
+    // Inverted Full Pyramid with Space ---------------->
+    for(int j =0 ;j<2*n -2*i-1;j++){
+        cout <<" ";
+    }
+     // Inverted half pyramid---------------->
+    for(int j =0 ;j<i+1;j++){
+        cout <<"*";
+    }
+    cout << endl;
+ }
 }
