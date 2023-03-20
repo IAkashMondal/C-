4b4c478b5992a491d88af2b152a13793044519fb
IAkashMondal <adas085322gmail.com>
2023-03-21 00:22:11 +0530

    Peak_Elem_in_Mountain_Arr_Binary Search

---
+++ 04_Searching&Sorting/05_Peake_Elem_In_Mountain.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int Peake_Elem_In_Mountain_Arr(vector<int>arr){
+    int ans= -1;
+    int start =0;
+    int end =arr.size()-1;
+    int mid =start+(end-start)/2;
+    while (start<end)
+    {
+        if(arr[mid]<arr[mid+1]){
+            start =mid+1;
+        }
+        else{
+            end= mid;
+            ans =arr[mid];
+        }
+        mid= start+(end-start)/2;
+    }
+    // return end;
+    return ans;
+    
+}
+int main(){
+
+    vector<int>arr{0,3,10,5,2};
+    cout<<  Peake_Elem_In_Mountain_Arr(arr)<< endl;
+    return 0;
+}
