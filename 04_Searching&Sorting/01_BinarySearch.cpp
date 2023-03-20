7efc86d61ecadc9d39b016b0508fed23d56d805b
IAkashMondal <adas085322gmail.com>
2023-03-20 21:24:06 +0530

    inbuild binary search

--- 04_Searching&Sorting/01_BinarySearch.cpp
+++ 04_Searching&Sorting/01_BinarySearch.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int BinarySearch(int arr[],int n, int target){
     int left =0;
     int right= n-1;
-    int mid =(left+right)/2;
+    int mid =left+(right-left)/2;
     while(left<=right){
         int elem =arr[mid];
         if(elem == target){
@@ -18,7 +20,7 @@ int BinarySearch(int arr[],int n, int target){
             // search in right side-------------------->
             left=mid+1;
         }
-         mid =(left+right)/2;
+         mid = left+(right-left)/2;
     }
 
     return -1;
@@ -29,6 +31,7 @@ int main(){
     cout<<"Enter Tagert Value"<<endl;
     cin >> target;
     int arr[]={1,3,5,7,9,11,14,17,29,20};
+     vector <int>arr1{1,3,5,7,9,11,14,17,29,20};
     int n = sizeof(arr) / sizeof(arr[0]);
      int result = BinarySearch(arr, n, target);
 
@@ -37,6 +40,19 @@ int main(){
     } else {
         cout << "Target found at index " << result << endl;
     }
-
+//  c++ method------------>
+    if(binary_search (arr1.begin(), arr1.end(), target)){
+        cout <<"Found in Vector " <<target<<endl;
+    }
+    else{
+        cout<<"Not Found in vector"<<endl;
+    }
+//  with out vector----------------- pointers need--------------->
+     if(binary_search (arr, arr+n, target)){
+        cout <<"Found in arr " <<target<<endl;
+    }
+    else{
+        cout<<"Not Found in arr"<<endl;
+    }
     return 0;
 }
