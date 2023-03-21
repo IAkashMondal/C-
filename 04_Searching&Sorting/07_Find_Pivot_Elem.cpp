8fe9302f54a0ecb7e7e96fdff228bdabcd10be76
IAkashMondal <adas085322gmail.com>
2023-03-22 00:03:04 +0530

    Find Pivit Elem

---
+++ 04_Searching&Sorting/07_Find_Pivot_Elem.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int Find_Pivot_Elem(vector <int>arr){
+    int start=0;
+    int end= arr.size()-1;
+    int mid = start+(end-start)/2;
+    while (start<=end){
+        if(arr.size()-1 && arr[mid]>arr[mid+1]){
+            return mid;
+        }
+        else if( arr[mid-1]>=0 && arr[mid-1]>arr[mid]){
+            return mid -1;
+        }
+        else if(arr[start]>= arr[mid]){
+            end =mid-1;
+        }
+        else if(arr[start]<= arr[mid]){
+            start = mid+1;
+        }
+        mid = start+(end-start)/2;
+    }
+    return -1;
+    
+}
+
+int main(){
+    cout<<"Consideing Piviot : 7----------------------------------------> "<< endl;
+    vector <int>arr{1,2,3,4,7};
+    int result = Find_Pivot_Elem(arr);
+    
+    if(result == -1){
+         cout<<"Not Found"<<endl;
+    }
+    else{
+        cout <<"foundv"<<result<<endl;
+    }
+
+    return 0;
+}
