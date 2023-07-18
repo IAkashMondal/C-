4ab8ca3f2cfe6b2160038b95d613cdd8326a885e
IAkashMondal <adas085322gmail.com>
2023-07-18 13:14:59 +0530

    reverse the arr

---
+++ Pact_2/01_Arrays/07_Reverse_arr.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+vector<int>arr1{1,2,3,4,5};
+vector<int>ans;
+int size =arr1.size()-1;
+for(int i= size; i>=0;i--){
+    cout <<arr1[i]<<" ";
+}
+
+cout<< endl;
+return 0;
+}
