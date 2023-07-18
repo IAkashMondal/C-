9996dfeed22e967728881c58202f199dc1b98b27
IAkashMondal <adas085322gmail.com>
2023-07-18 12:48:01 +0530

    min element in arr

--- Pact_2/01_Arrays/04_maximum_numbers_in_arr.cpp
+++ Pact_2/01_Arrays/04_maximum_numbers_in_arr.cpp
@@ -7,15 +7,15 @@ int main(){
 
 vector<int>arr1{1,2,3,4,5,28,54,13,-13,82,15};
 vector<int>ans;
-int max=INT_MIN;
-cout<<max<<endl;
+int min=INT_MAX;
+
 int size= arr1.size();
 for(int i=0;i<size;i++){
-    if(arr1[i]>max){
-        max=arr1[i];
+    if(arr1[i]<min){
+        min=arr1[i];
     }
 }
-cout<<max<<endl;
+cout<<min<<endl;
 cout<< endl;
 return 0;
 }
