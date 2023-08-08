8db54be33f305ca4d7d42714a2eb5525c25dc999
IAkashMondal <adas085322gmail.com>
2023-08-08 12:23:43 +0530

    last occourance in Bio search

---
+++ Pact_2/02_Searching_sort/04_Last_occurance.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    vector<int> arr{1, 2, 3, 3, 4, 5, 4, 4, 7};
+    int ans = -1;
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid = start + (start + end) / 2;
+    int target;
+    cout << "enter target" << endl;
+    cin >> target;
+    while (start <= end)
+    {
+
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            // search in left -->
+            start = mid + 1;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+
+    cout << ans << endl;
+    return 0;
+}
