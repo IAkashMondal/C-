e6107970966a4c5f842e6f9f05a6258028e96550
IAkashMondal <adas085322gmail.com>
2023-08-17 10:51:07 +0530

    remove all adjacent str

---
+++ 05_CharArrays&Strings/07_delete_all_adj_char.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void removeAllAdjElem(string input)
+{
+    int i = 0;
+    int n = input.length();
+    string ans = "";
+    while (i < n)
+    {
+        if (ans.length() > 0)
+        {
+            if (ans[ans.length() - 1] == input[i])
+            {
+                ans.pop_back();
+            }
+            else
+            {
+                ans.push_back(input[i]);
+            }
+        }
+        else
+        {
+            ans.push_back(input[i]);
+        }
+        i++;
+    }
+     cout << "Modified String: " << ans << endl;
+}
+int main()
+{
+
+    string input;
+    cout << "Enter String" << endl;
+    getline(cin, input);
+
+    removeAllAdjElem(input);
+
+        return 0;
+}
